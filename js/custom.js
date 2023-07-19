af63f8f51f43a3668bcd02fc9439d6fab2d46174
yunkyung <133613667+Yunkyungii@users.noreply.github.com>
2023-07-19 18:21:06 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -28,10 +28,48 @@ $(function () {
         $(this).addClass('on').siblings().removeClass('on');
     });
 
+
+
+
     const BenefitSlide = new Swiper('.benefit_slide', {
+        loop: true,
         slidesPerView: 3,
         spaceBetween: 32,
+    });
+
+    const AreaSlide = new Swiper('.area_slide', {
         loop: true,
+        effect: "fade",
+    });
+
+
+    $('.area_info li').on('mouseenter', function (e) {
+        e.preventDefault();
+        const idx = $(this).index();
+        $(this).addClass('on').siblings().removeClass('on')
+        AreaSlide.slideTo(idx);
     })
+
+
+
+
+    $(window).on('scroll', function () {
+        let sct = $(window).scrollTop();
+        $('.scr_ev').each(function () {
+            if (sct + $(window).innerHeight() - 200 > $(this).offset().top) {
+                $(this).addClass('on')
+            }
+            else {
+                $(this).removeClass('on')
+            };
+
+        })
+
+
+
+    });
+
+
+
 
 })
